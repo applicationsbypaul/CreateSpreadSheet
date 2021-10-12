8cdd25568c4b9c4682785edc8e4b9f1c3debcb70
Ford, Paul <pford@hy-vee.com>
2021-10-12 09:37:33 -0500

    Created Method for Sheets and the sheet to be added separately from the constructor.

--- CreateSpreadSheet/CreateSpreadSheet.cs
+++ CreateSpreadSheet/CreateSpreadSheet.cs
@@ -1,4 +1,5 @@
-﻿using DocumentFormat.OpenXml;
+﻿using System.Collections.Generic;
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 
@@ -9,6 +10,9 @@ namespace CreateSpreadSheet
         private static SpreadsheetDocument _spreadsheetDocument;
         private static WorkbookPart _workbookPart;
         private static WorksheetPart _worksheetPart;
+        private static Sheets _sheets;
+        private static Sheet _sheet;
+        
 
         private static readonly string[] Header =
         {
@@ -18,52 +22,44 @@ namespace CreateSpreadSheet
         public CreateSpreadSheet()
         {
             const string path =
-                @"C:\Users\pford\Desktop\Developer\CreateSpreadSheet\CreateSpreadSheet\constructorTest.xlsx";
-            CreateSreedsheetDocument(path);
+                @"C:\Users\pford\Desktop\Developer\CreateSpreadSheet\CreateSpreadSheet\ExcelTest\constructorTest.xlsx";
+            CreateSpreadsheetDocument(path);
             AddWorkBookToDocument();
             AddWorkSheetPartToWorkBookPart();
+            CreateSheets();
             CreateSpreadsheetWorkbook();
         }
-
-
-        private void CreateSreedsheetDocument(string filepath)
+        private static void CreateSpreadsheetDocument(string filepath)
         {
             _spreadsheetDocument = SpreadsheetDocument.Create(filepath, SpreadsheetDocumentType.Workbook);
         }
 
-        private void AddWorkBookToDocument()
+        private static void AddWorkBookToDocument()
         {
             _workbookPart = _spreadsheetDocument.AddWorkbookPart();
             _workbookPart.Workbook = new Workbook();
         }
 
-        private void AddWorkSheetPartToWorkBookPart()
+        private static void AddWorkSheetPartToWorkBookPart()
         {
             _worksheetPart = _workbookPart.AddNewPart<WorksheetPart>();
             _worksheetPart.Worksheet = new Worksheet(new SheetData());
         }
 
-        private static void CreateHeader(string[] header, Row row)
+        private static void CreateSheets()
         {
-            foreach (var headerTitle in header)
+            _sheets = _spreadsheetDocument.WorkbookPart.Workbook.AppendChild((new Sheets()));
+            _sheet = new Sheet()
             {
-                var cell = new Cell
-                {
-                    CellValue = new CellValue(headerTitle),
-                    DataType = CellValues.String
-                };
-                row.Append(cell);
-            }
+                Id = _spreadsheetDocument.WorkbookPart.GetIdOfPart(_worksheetPart),
+                SheetId = 1,
+                Name = "Constructor Sheet"
+            };
+            _sheets.Append(_sheet);
         }
 
         private static void CreateSpreadsheetWorkbook()
         {
-            var sheets = _spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
-            
-            var sheet = new Sheet()
-                {Id = _spreadsheetDocument.WorkbookPart.GetIdOfPart(_worksheetPart), SheetId = 1, Name = "mySheet"};
-            sheets.Append(sheet);
-
             var row = new Row();
             var row2 = new Row();
 
@@ -75,9 +71,22 @@ namespace CreateSpreadSheet
             }
 
             CreateHeader(Header, row);
-            
+
             _workbookPart.Workbook.Save();
             _spreadsheetDocument.Close();
+        }
+
+        private static void CreateHeader(IEnumerable<string> header, Row row)
+        {
+            foreach (var headerTitle in header)
+            {
+                var cell = new Cell
+                {
+                    CellValue = new CellValue(headerTitle),
+                    DataType = CellValues.String
+                };
+                row.Append(cell);
+            }
         }
     }
 }
