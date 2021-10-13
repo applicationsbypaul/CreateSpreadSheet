cb22c911974865539abbf078e2e129dc785ec016
PaulFord <applicationsbypaul@gmail.com>
2021-10-13 01:51:50 -0500

    I got the columns to work, Each method is created in order for assembling it correctly.

--- CreateSpreadSheet/CreateSpreadSheet.cs
+++ CreateSpreadSheet/CreateSpreadSheet.cs
@@ -18,10 +18,12 @@ namespace CreateSpreadSheet
         public CreateSpreadSheet()
         {
             const string path =
-                @"C:\Users\pford\Desktop\Developer\CreateSpreadSheet\CreateSpreadSheet\constructorTest.xlsx";
+                @"C:\Users\paulf\RiderProjects\CreateSpreadSheet\CreateSpreadSheet\constructorTest.xlsx";
             CreateSreedsheetDocument(path);
-            AddWorkBookToDocument();
+            AddWorkBookAndSheetsToDocument();
             AddWorkSheetPartToWorkBookPart();
+            AddColumnsToWorkSheet();
+            AddRows();
             CreateSpreadsheetWorkbook();
         }
 
@@ -31,18 +33,60 @@ namespace CreateSpreadSheet
             _spreadsheetDocument = SpreadsheetDocument.Create(filepath, SpreadsheetDocumentType.Workbook);
         }
 
-        private void AddWorkBookToDocument()
+        private void AddWorkBookAndSheetsToDocument()
         {
             _workbookPart = _spreadsheetDocument.AddWorkbookPart();
+            _worksheetPart = _workbookPart.AddNewPart<WorksheetPart>();
             _workbookPart.Workbook = new Workbook();
+
+            var sheets = new Sheets();
+            var workSheetPartID = _workbookPart.GetIdOfPart(_worksheetPart);
+            var sheet = new Sheet()
+                { Id = workSheetPartID, SheetId = 1, Name = "mySheet" };
+            var sheet2 = new Sheet()
+                { Id = workSheetPartID, SheetId = 2, Name = "multi-sheet" };
+            sheets.Append(sheet, sheet2);
+
+            _workbookPart.Workbook.Append(sheets);
         }
 
         private void AddWorkSheetPartToWorkBookPart()
         {
-            _worksheetPart = _workbookPart.AddNewPart<WorksheetPart>();
-            _worksheetPart.Worksheet = new Worksheet(new SheetData());
+            _worksheetPart.Worksheet = new Worksheet();
         }
 
+        private void AddColumnsToWorkSheet()
+        {
+            var columns = new Columns();
+            var column = new Column()
+            {
+                Min = 1,
+                Max = 1,
+                Width = 50,
+                CustomWidth = true
+            };
+            var column2 = new Column()
+            {
+                Min = 2,
+                Max = 2,
+                Width = 25,
+                CustomWidth = true
+            };
+
+            columns.Append(column, column2);
+            _worksheetPart.Worksheet.Append(columns);
+        }
+
+        private void AddRows()
+        {
+            var row = new Row();
+            var row2 = new Row();
+            CreateHeader(Header, row);
+            var sheetData = new SheetData();
+            sheetData.Append(row, row2);
+            _worksheetPart.Worksheet.Append(sheetData);
+        }
+        
         private static void CreateHeader(string[] header, Row row)
         {
             foreach (var headerTitle in header)
@@ -58,24 +102,6 @@ namespace CreateSpreadSheet
 
         private static void CreateSpreadsheetWorkbook()
         {
-            var sheets = _spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
-            
-            var sheet = new Sheet()
-                {Id = _spreadsheetDocument.WorkbookPart.GetIdOfPart(_worksheetPart), SheetId = 1, Name = "mySheet"};
-            sheets.Append(sheet);
-
-            var row = new Row();
-            var row2 = new Row();
-
-            var sheetData = _worksheetPart.Worksheet.GetFirstChild<SheetData>();
-            if (sheetData != null)
-            {
-                sheetData.Append(row);
-                sheetData.Append(row2);
-            }
-
-            CreateHeader(Header, row);
-            
             _workbookPart.Workbook.Save();
             _spreadsheetDocument.Close();
         }
